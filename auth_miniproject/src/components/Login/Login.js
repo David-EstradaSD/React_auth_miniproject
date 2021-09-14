6f04e16f0d13fe94ad60c59a208f3341f37e6313
David <destrada2193@gmail.com>
2021-09-14 14:13:02 -0700

    Add useReducer w/ object destructuring logic

--- auth_miniproject/src/components/Login/Login.js
+++ auth_miniproject/src/components/Login/Login.js
@@ -1,59 +1,102 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+const emailReducer = (state, action) => { // This is our Reducer function. What we dispatch as an "action" will be an object per what we define in our dispatch function below  
+  if (action.type === 'USER_INPUT') {
+    return { value: action.val, isValid: action.val.includes('@') };
+  }
+  if (action.type === 'INPUT_BLUR') {
+    return { value: state.value, isValid: state.value.includes('@') };
+  }
+  return {value: '', isValid: false};
+};  // this reducer function is outside the component function since we won't need any data generated inside the compnoent function 
+
+const passwordReducer = (state, action) => {
+  if (action.type === 'USER_INPUT') {
+    return { value: action.val, isValid: action.val.trim().length > 6 };
+  }
+  if (action.type === 'INPUT_BLUR') {
+    return { value: state.value, isValid: state.value.trim().length > 6 };
+  }
+  return {value: '', isValid: false};
+
+};
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
+
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '', 
+    isValid: null});
+
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: '', 
+    isValid: null,
+
+  });
 
   useEffect(() => {
     console.log('Effect running');
 
       return () => {
-
+        console.log('EFFECT CLEANUP');
       };
   }, []);
 
+  const { isValid: emailIsValid } = emailState; // using object destructuring to pull out certain properties of object
+  const { isValid: passwordIsValid } = passwordState;
+  // with both these lines^, we're pulling out the isValid properties and assigning an alias ( note the curly braces on the left hand side of equal sign )
+
   useEffect(() => {
     const identifier = setTimeout(() => {
-      console.log('Checking form validity!');
-      setFormIsValid( // this code will only run once 
-        enteredEmail.includes('@') && enteredPassword.trim().length > 6
-      ); 
-    }, 500);
+      console.log('Checking form vailidity')
+      setFormIsValid(emailIsValid && passwordIsValid        
+    // now that setFormIsValid is inside useEffect, this will run with the latest state values!
+    // this will run with every state update that React returns 
+      );
+    }, 500); // our timeout duration is 5 millaseconds 
 
     return () => {
-      console.log('Cleanup');
-      clearTimeout(identifier); // we clear the last timer before we set a new timer 
+      console.log('CLEANUP')
+      clearTimeout(identifier);
     };
-  }, [enteredEmail, enteredPassword]); // we want to re-evaluate this setFormIsValid with every keystroke
-  // rule of thumb: we add as dependencies, what we're using in our useEffect function aka (setFormIsValid, enteredEmail, enteredPassword)
-  // if any of the 3 change
+  }, [emailIsValid, passwordIsValid]);
 
-  const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+  const emailChangeHandler = (event) => { 
+    dispatchEmail({type: 'USER_INPUT', val: event.target.value}); 
+    // dispatchEmail is our "dispatch" function for our useReducer 
+    // this has some identifier with all caps by convention and an extra payload (in this case, the value the user entered as "val")
+
+  //   setFormIsValid(
+  //     event.target.value.includes('@') && passwordState.isValid
+  //   );
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+    dispatchPassword({type: 'USER_INPUT', val: event.target.value})
+
+    setFormIsValid(emailState.isValid && event.target.value.trim().length > 6
+    );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({type: 'INPUT_BLUR'})
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispatchPassword({type: 'INPUT_BLUR'});
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -61,28 +104,28 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
